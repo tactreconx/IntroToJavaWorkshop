87bcc51a77e663c2bcf1fdd71999c309e827fa08
tactreconx <league@Mac-mini-2.local>
2016-03-10 18:28:50 -0800

    Today,I learned how to make the world domination program.

---
+++ src/day3/Theriddler.java
@@ -0,0 +1,34 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class Theriddler {
+
+	public static void main(String[] args) {
+		int score=0;
+		String Rule =JOptionPane.showInputDialog("What room can no one enter?");
+		if (Rule.equals("A Mushroom."))
+		{
+	
+			JOptionPane.showInputDialog("Your Right!");
+			if (Rule.equals(I do not know))
+		}
+		
+		// 1. Make a variable to hold the score
+
+		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+
+		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+
+		// 5. Otherwise, say "wrong" and tell them the answer
+
+		// 6. Add some more riddles
+
+		// 2. Make a pop up to show the score.
+		
+			
+
+
+
+
+
--- src/day3/WorldDomination.java
+++ src/day3/WorldDomination.java
@@ -4,6 +4,15 @@ import javax.swing.JOptionPane;
 
 public class WorldDomination {
 	public static void main(String[] args) {
+		String gar =JOptionPane.showInputDialog("Do you know how to write code?");
+		if (gar.equals("yes"))
+			{
+			JOptionPane.showMessageDialog(null,"YOU RULE THE WORLD!!");
+			
+			}
+		else{
+		JOptionPane.showMessageDialog(null, "Good luck washing dishes");
+		}
 		// 1. Ask the user if they know how to write code.
 
 		// 2. If they say "yes", tell them they will rule the world.
@@ -12,4 +21,6 @@ public class WorldDomination {
 
 	}
 }
+
+
 
